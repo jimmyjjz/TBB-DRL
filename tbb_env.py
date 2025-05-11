f272ee40c74c2e3cdd621ec35eef378d26e8bfdf
Jimmy Z <JimmyJunjiZhu@gmail.com>
2025-05-10 23:18:49 -0400

    Terraria boss battle environment

---
+++ tbb_env.py
@@ -0,0 +1,35 @@
+import gymnasium as gym
+from gymnasium import spaces
+import pyautogui
+import numpy as np
+import dxcam
+import easyocr
+from utils import resize_img
+
+HEIGHT=1080
+WIDTH=1920
+RESCALE_FACTOR=8
+#.//resize_img(WIDTH//RESCALE_FACTOR, HEIGHT//RESCALE_FACTOR)
+
+class TBBEnv(gym.Env):
+    def __init__(self):
+        super().__init__()
+        self.action_space = gym.spaces.Tuple((
+            gym.spaces.Discrete(3), # left, right, jump
+            gym.spaces.Box(low=0, high=WIDTH, shape=(1,), dtype=int), # horizontal mouse placement
+            gym.spaces.Box(low=0, high=HEIGHT, shape=(1,), dtype=int) # vertical mouse placement
+        ))
+        self.observation_space = spaces.Box(0,255,shape=(HEIGHT//RESCALE_FACTOR, WIDTH//RESCALE_FACTOR, 3), dtype=np.uint8)
+        self.step=0
+        self.episode_steps=0
+        self.screen_grabber = dxcam.create(output_idx=0)
+        self.text_reader = easyocr.Reader(['en'])
+
+    def reset(self):
+	pass
+
+    def step(self, action):
+        pass
+
+    def render(self):
+        pass
